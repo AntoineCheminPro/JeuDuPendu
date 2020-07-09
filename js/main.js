b1412cc853a81df166ce5dad19acc34ee63b9205
AntoineCheminPro <antoine.chemin.pro@outlook.fr>
2020-07-09 17:20:11 +0200

    mise en forme du mot choisi en mot masqué

--- js/main.js
+++ js/main.js
@@ -5,8 +5,19 @@ alert ("Bienvenue sur le jeu du pendu");
 const listeDeMots = ["bienvenue", "brouillon", "carreau", "dragon", "esperluette", "facile"];
 
 // random an index and select it in words array
-let indexDuMot = Math.floor(Math.random() * (listeDeMots.length - 0 +1));
-let motChoisi = listeDeMots[indexDuMot];
+function choixDuMot (){
+    let indexDuMot = Math.floor(Math.random() * (listeDeMots.length - 0 +1));
+    let motAuHasard = listeDeMots[indexDuMot];
+return motAuHasard;
+}
 
+// transformation du mot au hasard en chaine de caractères puis en "_"
+let motEnLettres = choixDuMot().split('');
+let motMasque = "";
+ for (let i = 0 ; i < motEnLettres.length ; i++){
+    motMasque += "_ "};
 
-console.log( motChoisi);
+// affichage du mot masqué et demande proposition de lettre    
+let propositionDeLettre = prompt (`${motMasque} \n quelle lettre proposes-tu?`);
+console.log(motEnLettres);
+
