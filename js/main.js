90913b10c1c14f806b27b30b7a8d572509f73583
AntoineCheminPro <antoine.chemin.pro@outlook.fr>
2020-07-10 11:07:13 +0200

    able to switch underscore with good choice in case of

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@
 // verifie que la saisie est une lettre et une seule
 // affichage du mot masqué et demande proposition de lettre
 function estUneLettre(proposition) {
-    while (proposition.length !== 1 && !proposition.match(/[a-z]/i)){
+    do {
         proposition = prompt (`${motMasque} \n quelle lettre proposes-tu?`);
     }
-    return proposition;
+    while (proposition.length !== 1 && !proposition.match(/[a-z]/i));
+return proposition;
 }
+
+
 // random an index and select it in words array
 function choixDuMot (){
     let indexDuMot = Math.floor(Math.random() * (listeDeMots.length - 0 +1));
@@ -22,11 +25,7 @@ return motAuHasard;
 const listeDeMots = ["bienvenue", "brouillon", "carreau", "dragon", "esperluette", "facile", "fonction", "migraine"];
 
 // recensement des lettres déjà jouées
-const lettreTrouvee = [];
-const lettreProposee = [];
-const motChoisi = choixDuMot();
-
-let motMasque = "";
+const MOTCHOISI = choixDuMot();
 
 // le jeu
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -35,20 +34,25 @@ let motMasque = "";
 alert ("Bienvenue sur le jeu du pendu");
 
 // transformation du mot au hasard en chaine de caractères
-// puis en "_" ou en lettre affichée si le joueur à vu juste
-let motEnLettres = motChoisi.split('');
+let motEnLettres = MOTCHOISI.split('');
 
-let vieSauve = 0;
-let propositionDeLettre = prompt (`${motMasque} \n quelle lettre proposes-tu?`); 
+let motMasque = "";
+for (let i in motEnLettres){
+    motMasque += " _";
+}
+// ask for a letter and switch it to lower case as words are
+let propositionDeLettre = estUneLettre().toLowerCase();
+console.log(propositionDeLettre);
 
- for (let i = 0 ; i < motEnLettres.length ; i++){
-    if (estUneLettre(propositionDeLettre) === motEnLettres[i]){
-        motMasque += propositionDeLettre;
-        vieSauve += 1;
-    }
-    else {
-    motMasque += "_ "
-    }
- }
+// switch string motMasque to array
+motMasque = motMasque.split('');
+// for each value in motEnLettre input the new letter in case it was a good choice
+for (let i = 0 ; i < motEnLettres.length ; i++){
+    (propositionDeLettre == motEnLettres[i] ?  motMasque[i] = propositionDeLettre : );
+    console.log(motEnLettres[i]);
+}
 
+// switch array to string
+motMasque = motMasque.join('');
 console.log(motEnLettres);
+console.log(motMasque);
