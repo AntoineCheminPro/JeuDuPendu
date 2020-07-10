f777b356d70b150b6abd9e4ba88fe2f50c1b2afe
AntoineCheminPro <antoine.chemin.pro@outlook.fr>
2020-07-10 15:11:12 +0200

    console active pour j ou q mais bugguée

--- js/main.js
+++ js/main.js
@@ -11,70 +11,85 @@ function estUneLettre(proposition) {
 return proposition;
 }
 
-
 // random an index and select it in words array
 function choixDuMot (){
     let indexDuMot = Math.floor(Math.random() * (listeDeMots.length - 0 +1));
     let motAuHasard = listeDeMots[indexDuMot];
 return motAuHasard;
 }
+
+function reponse(stopOuEncore){
+    do {
+        stopOuEncore = prompt ("Entre j pour jouer \n r pour voir les régles \n q pour quitter").toLowerCase();
+    }
+    while (stopOuEncore.length !== 1 && !stopOuEncore.match(/[a-z]/i));
+return stopOuEncore;
+}
+
 // Constantes
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 // create a word list
-const listeDeMots = ["bienvenue", "brouillon", "carreau", "dragon", "esperluette", "facile", "fonction", "migraine"];
-
-// make the word to find as constant
-const MOTCHOISI = choixDuMot();
+const listeDeMots = ["bienvenue", "brouillon", "carreau", "dragon", "esperluette", "facile", "fonction", "migraine","solution","pendu","horloge"];
 
 // the Game
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let essaiRestant;
+let motMasque;
+let stopOuEncore;
 
-// welcome alert
-alert ("Bienvenue sur le jeu du pendu");
+while (reponse(stopOuEncore) == "j") {
+    // reponse = prompt ("Entre j pour jouer \n r pour voir les régles \n q pour quitter").toLowerCase();
+    console.log (reponse());
+    // welcome alert
+    alert ("Bienvenue sur le jeu du pendu");
 
-let essaiRestant = 7;
+    essaiRestant = 7;
+    motMasque = "";
 
-// translate the word in an array in order to work with it
-let motEnLettres = MOTCHOISI.split('');
+    // make the word to find as constant
+   let motChoisi = choixDuMot();
+console.log(motChoisi);   
+        
+    // translate the word in an array in order to work with it
+    let motEnLettres = motChoisi.split('');
 
-let motMasque = "";
-for (let i in motEnLettres){
-    motMasque += "_";
-}
-
-// loop start here since win or loose
-do {
-    // ask for a letter and switch it to lower case as words are
-    let propositionDeLettre = estUneLettre().toLowerCase();
-
-    // switch string motMasque to array
-    motMasque = motMasque.split('');
-
-    // let a variable for evaluation of succes in finding a letter
-    let tentative = motEnLettres.length;
-
-    // for each value in motEnLettre input the new letter in case it was a good choice
-    for (let i = 0 ; i < motEnLettres.length ; i++){
-        (propositionDeLettre == motEnLettres[i] ?  motMasque[i] = propositionDeLettre : tentative -= 1 );
-    }
-    // switch array to string
-    motMasque = motMasque.join('');
     
-    // tentative decrease by one point each time letter is not find, so it's positive number each time the letter choosed is in the word
-    if(tentative === 0){
-        essaiRestant = essaiRestant - 1;
+    for (let i in motEnLettres){
+        motMasque += "_";
     }
-}
-while (motMasque !== MOTCHOISI && essaiRestant !== 0);
 
+    // loop start here since win or loose
+    do {
+        // ask for a letter and switch it to lower case as words are
+        let propositionDeLettre = estUneLettre().toLowerCase();
 
+        // switch string motMasque to array
+        motMasque = motMasque.split('');
 
-if (essaiRestant === 0) {
-    alert (`pendu ! Le mot à trouver était : ${MOTCHOISI}.`)
-}
-else{
-    alert(`BRAVO !  Le mot à trouver était : ${MOTCHOISI}. \n Il te restait ${essaiRestant} essai(s).`)
+        // let a variable for evaluation of succes in finding a
+        let tentative = motEnLettres.length;
+        // for each value in motEnLettre input the new letter in case it was a good choice
+        for (let i = 0 ; i < motEnLettres.length ; i++){
+            (propositionDeLettre == motEnLettres[i] ?  motMasque[i] = propositionDeLettre : tentative -= 1 );
+        }
+        // switch array to string
+        motMasque = motMasque.join('');
+        // tentative decrease by one point each time letter is not find, so it's positive number each time the letter choosed is in the word
+        if(tentative === 0){
+            essaiRestant = essaiRestant - 1;
+        }
+    }
+    while (motMasque !== motChoisi && essaiRestant !== 0);
+
+    // finishing the game
+    if (essaiRestant === 0) {
+        alert (`pendu ! Le mot à trouver était : ${motChoisi}.`)
+    }
+    else{
+        alert(`BRAVO !  Le mot à trouver était : ${motChoisi}. \n Il te restait ${essaiRestant} essai(s).`)
+    }
+    reponse();
+
 }
 
 
